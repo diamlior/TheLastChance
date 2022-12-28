fcbecd9a8604d7c613c184a896df22061157b491
Lior Diamant <diamlior@gmail.com>
2022-12-28 12:51:45 +0200

    First commit with .gitignore

---
+++ Assets/Movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public GameObject player;
+    public Rigidbody rb;
+    public float jumpForce = 5;
+    public float movementForce = 2;
+    bool isGrounded = true;
+    Renderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = player.GetComponent<Renderer>();
+
+    }
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+            player.transform.position += Vector3.right * Time.deltaTime * movementForce;
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            player.transform.position += Vector3.left * Time.deltaTime * movementForce;
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+
+        }
+        if (collision.collider.name != "Floor")
+        {
+            renderer.material.color = new Color(255, 255, 255);
+        }
+
+    }
+}
