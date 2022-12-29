3c9c75785ecd9e7d1332cfcde1ba7bffab203120
Lior Diamant <diamlior@gmail.com>
2022-12-29 20:35:58 +0200

    Added small mechanic and football pattern

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Movement : MonoBehaviour
 {
     public GameObject player;
     public Rigidbody rb;
     public float jumpForce = 5;
-    public float movementForce = 2;
+    public float speed = 100;
     bool isGrounded = true;
+    bool inMovement = false;
+    float currentX, targetX;
     Renderer renderers;
     // Start is called before the first frame update
     void Start()
@@ -21,18 +24,34 @@ public class Movement : MonoBehaviour
         isGrounded = true;
     }
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
-            player.transform.position += Vector3.right * Time.deltaTime * movementForce;
-        else if (Input.GetKey(KeyCode.LeftArrow))
-            player.transform.position += Vector3.left * Time.deltaTime * movementForce;
+        if (Input.GetKey(KeyCode.RightArrow) && !inMovement)
+        {
+            rb.AddForce(Vector3.right * speed);
+            currentX = (float)System.Math.Round(player.GetComponent<Transform>().position.x);
+            targetX = currentX + 1f;
+            inMovement = true;
+        }
+
+        else if (Input.GetKey(KeyCode.LeftArrow) && !inMovement)
+        {
+            rb.AddForce(Vector3.left * speed);
+            currentX = (float)System.Math.Round(player.GetComponent<Transform>().position.x);
+            targetX = currentX - 1f;
+            inMovement = true;
+        }
+        else if (inMovement && System.Math.Abs(player.GetComponent<Transform>().position.x - targetX) < 0.05f) {
+            inMovement = false;
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
         }
-
+        currentX = (float)System.Math.Round(player.GetComponent<Transform>().position.x);
     }
 
     void OnCollisionEnter(Collision collision)
@@ -44,7 +63,7 @@ public class Movement : MonoBehaviour
         }
         if (collision.collider.name != "Floor")
         {
-            renderers.material.color = new Color(255, 255, 255);
+            renderers.material.color = new Color(0, 0, 0);
         }
 
     }
