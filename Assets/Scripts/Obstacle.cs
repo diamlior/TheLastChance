1f115a9c331875b7e81f3ccd4f83343c50b7c347
Tal-Yoffe <yoffe.tal@gmail.com>
2023-01-03 17:58:49 +0200

    Main Menu scene

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform obstacle;
+    public GameObject obstacle_go;
+    public float speed = 1;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        obstacle.position += Vector3.back * Time.deltaTime * speed;
+    }
+}
