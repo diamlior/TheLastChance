b7b39c338b84d8e3593f5697069eb30cd5cb4f3a
Lior Diamant <diamlior@gmail.com>
2023-01-09 11:19:13 +0200

    Fix of last changes

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour
+{
+    // The force to apply to the ball when shooting it
+    public float Force;
+    // Target is for the player to direct his shot
+    public Transform target;
+    // forceUI is for the player to choose how strong to shoot the ball
+    public Slider forceUI;
+    private LevelChangerScript levelChangeScript;
+    
+    string sceneName = "";
+
+    private void Start()
+    {
+        levelChangeScript = GameObject.Find("LevelChanger").GetComponent<LevelChangerScript>();
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+    // The ball object
+    //public GameObject ball;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.Space)) //Fill slider depending on Force value
+        {
+            Force++;
+            slider();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space)) //shoot
+        {
+            shoot();
+            StartCoroutine(Wait());
+        }
+    }
+
+    void shoot()
+    {
+            Vector3 Shoot = (target.position - this.transform.position).normalized;
+            GetComponent<Rigidbody>().AddForce(Shoot * Force + new Vector3(0, 3f, 0), ForceMode.Impulse);
+    }
+
+    public void slider()
+    {
+        forceUI.value = Force;
+    }
+
+    public void ResetGauge()
+    {
+        Force = 0;
+        forceUI.value = 0;
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1.5f);
+        ResetGauge();
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+
+        }
+        string collider = collision.collider.name;
+        if (collider != "Floor")
+        {
+            if (collider.Equals("EndingBlock"))
+            {
+                string nextScene = "";
+                switch (sceneName)
+                {
+                    case "SampleScene":
+                        nextScene = "PenaltyScene";
+                        break;
+                    case "PenaltyScene":
+                        nextScene = "SampleScene";
+                        break;
+                    default:
+                        nextScene = "PenaltyScene";
+                        break;
+                }
+                levelChangeScript.FadeToLevel(nextScene);
+            }
+          
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Movement : MonoBehaviour
+{
+    public GameObject player;
+    public Rigidbody rb;
+    
+    public float jumpForce = 5;
+    public float speed = 100;
+    public bool isGrounded = true, stopJumped = false;
+    bool movingLeft = false, movingRight = false;
+    float currentX, targetX, startingX;
+    string sceneName;
+    Renderer renderers;
+    Transform transform;
+    private LevelChangerScript levelChangeScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderers = player.GetComponent<Renderer>();
+        transform = player.GetComponent<Transform>();
+        startingX = transform.position.x;
+        sceneName = SceneManager.GetActiveScene().name;
+        levelChangeScript = GameObject.Find("LevelChanger").GetComponent<LevelChangerScript>();
+    }
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) && !movingRight && currentX <= startingX)
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+            rb.angularVelocity = Vector3.zero;
+            rb.AddForce(Vector3.right * speed);
+            currentX = (float)System.Math.Round(transform.position.x);
+            targetX = currentX + 1f;
+            movingRight = true;
+            movingLeft = false;
+        }
+
+        else if (Input.GetKey(KeyCode.LeftArrow) && !movingLeft && currentX >= startingX)
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+            rb.angularVelocity = Vector3.zero;
+            rb.AddForce(Vector3.left * speed);
+            currentX = (float)System.Math.Round(transform.position.x);
+            targetX = currentX - 1f;
+            movingLeft = true;
+            movingRight = false;
+        }
+        else if ((movingLeft || movingRight) && System.Math.Abs(transform.position.x - targetX) < 0.05f) {
+            movingRight = false;
+            movingLeft = false;
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+            rb.angularVelocity = Vector3.zero;
+            currentX = (float)System.Math.Round(transform.position.x);
+            float y = transform.position.y;
+            float z = transform.position.z;
+            player.GetComponent<Transform>().position = new Vector3(currentX, y, z);
+        }
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+            stopJumped = false;
+        }
+        else if (!Input.GetKey(KeyCode.Space) && !isGrounded && !stopJumped)
+        {
+            if (rb.velocity.y > 0)
+                rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+            stopJumped = true;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+
+        }
+        string collider = collision.collider.name;
+        if (collider != "Floor")
+        {
+            if (collider.Equals("EndingBlock"))
+            {
+                string nextScene = "";
+                switch (sceneName)
+                {
+                    case "SampleScene":
+                        nextScene = "PenaltyScene";
+                        break;
+                    case "PenaltyScene":
+                        nextScene = "SampleScene";
+                        break;
+                    default:
+                        nextScene = "PenaltyScene";
+                        break;
+                }
+                levelChangeScript.FadeToLevel(nextScene);
+            }
+            else
+            {
+                Debug.Log("You are dead!");
+                levelChangeScript.FadeToLevel(sceneName);
+            }
+
+        }
+
+    }
+}
