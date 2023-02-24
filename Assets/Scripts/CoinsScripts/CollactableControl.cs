7ed838bc8ad522dec3e7186b78cff0a450c95c7b
Tal-Yoffe <yoffe.tal@gmail.com>
2023-02-24 18:13:57 +0200

    Reset score at death

--- Assets/Scripts/CoinsScripts/CollactableControl.cs
+++ Assets/Scripts/CoinsScripts/CollactableControl.cs
@@ -9,6 +9,11 @@ public class CollactableControl : MonoBehaviour
     public static int coinCount;
     public GameObject coinCountDisplay;
 
+    void Start()
+    {
+        coinCount = 0;
+    }
+
     void Update()
     {
         coinCountDisplay.GetComponent<TMP_Text>().text = "" + coinCount;
